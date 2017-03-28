1d3a8804bc2a15ec09bc0d0bf467993fed0b3c61
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-03-29 02:18:13 +0300

    Добавлен скрипт обработки формы обратной связи

--- partnership.php
+++ partnership.php
@@ -3,7 +3,7 @@
   $to = "aksenov.mailbox@gmail.com";
   $subject = "Новая заявка на сотрудничество";
   $subject = "=?utf-8?B?".base64_encode($subject)."?=";
-  $headers = "From:aksenov.mailbox@gmail.com\r\nReply-To:aksenov.mailbox@gmail.com\r\nContent-type:text/htmlcharset=utf-8\r\n";
+  $headers = "From:aksenov.mailbox@gmail.com\r\nReply-To:aksenov.mailbox@gmail.com\r\nContent-type:text/plain;charset=utf-8\r\n";
   
   $company = htmlspecialchars($_POST["partnership-form__company"]);
   $name = htmlspecialchars($_POST["partnership-form__name"]);
@@ -16,13 +16,21 @@
     exit;
   }
 
-  $message = "Здравствуйте!<br>
+  /*$message = "Здравствуйте!<br>
               Поступила новая заявка на сотрудничество.<br>
               Компания:<b>$company</b><br>
               Имя:<b>$name</b><br>
               Email:<b>$email</b><br>
               Телефон:<b>$phone</b><br>
-              Комментарий:<b>$comment</b><br>";
+              Комментарий:<b>$comment</b><br>";*/
+
+  $message = "Поступила новая заявка на сотрудничество\r\n
+Компания: $company\r\n
+Контактное лицо: $name\r\n
+Электронная почта: $email\r\n
+Контактный телефон: $phone\r\n
+Описание запроса: $comment\r\n";
+
   mail($to, $subject, $message, $headers);
   header("Location:success.html");
   exit;
